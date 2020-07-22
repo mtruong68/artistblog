c23dbee20352c28d98dd3c771c6333da1cc0a4a2
mtruong68 <marytruong2020@u.northwestern.edu>
2020-07-22 01:46:19 -0500

    so i messed up w the node modules

--- app/app.jsx
+++
@@ -1,7 +0,0 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-
-/* Import Components */
-const Article = require('./components/Article');
-
-ReactDOM.render(<Article/>, document.getElementById('main'));
