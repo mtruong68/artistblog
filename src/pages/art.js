7b107086f3f3a9aa0594dec74e7956fe09d76006
mtruong68 <marytruong2020@u.northwestern.edu>
2020-08-04 00:03:36 -0500

    art page

--- src/pages/art.js
+++ src/pages/art.js
@@ -2,35 +2,46 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import get from 'lodash/get'
 
-import { Helmet } from 'react-helmet'
-import Layout from '../components/layout'
 import { Link } from 'gatsby'
+import Img from 'gatsby-image'
+
+import styles from './art.module.css'
+import styled from 'styled-components';
+
+const SetImg = styled(Img)`
+  display: block;
+  flex-grow: 1;
+`;
 
 class ArtPage extends React.Component {
   render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
     const posts = get(this, 'props.data.allContentfulArticle.edges')
+    const logoLarge = get(this, 'props.data.allContentfulBranding.edges[0].node.logoLarge.fixed')
 
     return (
-      <Layout location={this.props.location}>
-        <div style={{ background: '#fff' }}>
-          <Helmet title={siteTitle} />
-          <div className="wrapper">
-            <h2 className="section-headline">Recent articles</h2>
-            <ul className="article-list">
-              {posts.map(({ node }) => {
-                return (
-                  <li key={node.slug}>
-                  <Link to={`/art/${node.slug}`}>
-                    {node.slug}
-                  </Link>
-                  </li>
-                )
-              })}
-            </ul>
+      <div>
+      <div>
+        <Link to={`/`}>
+        <Img fixed={logoLarge} />
+        </Link>
+      </div>
+          <div className={styles.articleList}>
+            {posts.map(({ node }) => {
+              return (
+                <Link to={`/art/${node.slug}`} key={`${node.slug}`}className={styles.articleContainer}>
+                  <div className={styles.imgWrapper}>
+                    <SetImg
+                    alt={node.carouselImage.title}
+                    fluid={node.carouselImage.fluid}
+                    imgStyle={{ objectFit: 'contain' }} />
+                  </div>
+                  <div className={styles.artistName}>{node.artistName}</div>
+                  <div className={styles.titleQuote}>{node.titleQuote.childMarkdownRemark.excerpt}</div>
+                </Link>
+              )
+            })}
           </div>
-        </div>
-      </Layout>
+      </div>
     )
   }
 }
@@ -45,12 +56,34 @@ export const pageQuery = graphql`
           artistName
           titleQuote {
             childMarkdownRemark {
-              html
+              excerpt
             }
           }
           slug
+          carouselImage{
+            title
+            fluid(maxWidth: 500) {
+              ...GatsbyContentfulFluid
+            }
+          }
         }
       }
     }
+    allContentfulBranding{
+      edges{
+        node{
+          logo{
+            fluid(maxWidth: 1200) {
+              ...GatsbyContentfulFluid
+            }
+          }
+          logoLarge{
+            fixed(width: 200) {
+              ...GatsbyContentfulFixed
+            }
+          }
+        }
+    }
   }
+}
 `
